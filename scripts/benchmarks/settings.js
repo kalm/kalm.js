ce6d1afe000734136b3bc5c2f6d992de1912764d
frederic charette <fredericcharette@gmail.com>
2020-03-27 19:49:47 -0400

    fixed benchmark time setting

--- scripts/benchmarks/settings.js
+++ scripts/benchmarks/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   transport: 'tcp',
   port: 3001,
   routine: ['dynamic', { hz: 200 }],
-  testDuration: 1000 * 1,
+  testDuration: 1000 * 10,
   testPayload: { foo: 'bar' },
   testChannel: 'test',
   framing: null,
