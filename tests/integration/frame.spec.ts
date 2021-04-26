d8c0f30a049b3a9984037d59ed32aa7f8dfd5f5e
Frederic Charette <fred@Frederics-MacBook-Pro.local>
2021-04-25 23:05:44 -0400

    fixed linting, build order

--- tests/integration/frame.spec.ts
+++ tests/integration/frame.spec.ts
@@ -1,4 +1,3 @@
-
 /* Requires ------------------------------------------------------------------*/
 
 import { connect, listen } from '../../packages/kalm/bin/kalm';
@@ -7,49 +6,49 @@ import ipc from '../../packages/ipc/bin/ipc';
 /* Suite --------------------------------------------------------------------*/
 
 describe('Frame', () => {
-    let server;
+  let server;
 
-      /* --- Setup ---*/
+  /* --- Setup ---*/
 
-      // Create a server before each scenario
-      beforeEach(() => {
-        server = listen({
-          transport: ipc(),
+  // Create a server before each scenario
+  beforeEach(() => {
+    server = listen({
+      transport: ipc(),
+    });
+  });
+
+  // Cleanup afterwards
+  afterEach(done => {
+    server.stop();
+    server = null;
+    setTimeout(() => done(), 100);
+  });
+
+  it('Should have a well structured frame reference', done => {
+    const payload = { foo: 'bar' };
+    server.on('connection', c => {
+      c.subscribe('test', (data, meta) => {
+        expect(meta).toEqual({
+          client: c,
+          frame: {
+            channel: 'test',
+            id: 0,
+            messageIndex: 0,
+            payloadBytes: 51,
+            payloadMessages: 1,
+          },
         });
+        done();
       });
+    });
+    server.on('error', e => {
+      throw new Error(e);
+    });
 
-      // Cleanup afterwards
-      afterEach(done => {
-        server.stop();
-        server = null;
-        setTimeout(() => done(), 100);
-      });
-
-      it('Should have a well structured frame reference', (done) => {
-        const payload = { foo: 'bar' };
-        server.on('connection', c => {
-          c.subscribe('test', (data, meta) => {
-            expect(meta).toEqual({
-                client: c,
-                frame: {
-                    channel: 'test',
-                    id: 0,
-                    messageIndex: 0,
-                    payloadBytes: 51,
-                    payloadMessages: 1
-                }
-            });
-            done();
-          });
-        });
-        server.on('error', e => {
-          throw new Error(e);
-        });
-
-        const client = connect({ transport: ipc() });
-        client.on('error', e => {
-          throw new Error(e);
-        });
-        client.write('test', payload);
-      });
+    const client = connect({ transport: ipc() });
+    client.on('error', e => {
+      throw new Error(e);
+    });
+    client.write('test', payload);
+  });
 });
